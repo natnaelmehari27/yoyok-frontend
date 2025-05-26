7940c9b031d5b94041c6b550266492f3493383e2
Natnael Mehari <natnaelmehari27@gmail.com>
2025-05-26 14:36:06 +0200

    import react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import Button from 'react-bootstrap/Button'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-       <h1>Hello, world!</h1>
+    <div className="App">
+       <Button variant="primary">Primary</Button>
+    </div>
   );
 }
 
