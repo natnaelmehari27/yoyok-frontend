f5a7109d1c6983a343f057cce26259c38918c8b2
Natnael Mehari <natnaelmehari27@gmail.com>
2025-06-06 23:28:52 +0200

    update NavBar for React Router active link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,33 +9,35 @@ function NavBar() {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <NavLink to="/">
-          <Navbar.Brand>
-            <img src={logo} alt="logo" height="45"></img>
+          <Navbar.Brand as={NavLink} to="/">
+            <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
-        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="ms-auto text-left">
             <NavLink
-              exact
-              className={styles.NavLink}
-              activeClassName={styles.Active}
               to="/"
+              end
+              className={({ isActive }) =>
+                `${styles.NavLink} ${isActive ? styles.Active : ""}`
+              }
             >
-              <i className="fas fa-home"></i>Home
+              <i className="fas fa-home" aria-hidden="true"></i>Home
             </NavLink>
+
             <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
               to="/signin"
+              className={({ isActive }) =>
+                `${styles.NavLink} ${isActive ? styles.Active : ""}`
+              } 
             >
-              <i className="fas fa-sign-in-alt"></i>sign in
+              <i className="fas fa-sign-in-alt" aria-hidden="true"></i>sign in
             </NavLink>
             <NavLink
-              className={styles.NavLink}
-              activeClassName={styles.Active}
               to="/signup"
+              className={({ isActive }) =>
+                `${styles.NavLink} ${isActive ? styles.Active : ""}`
+              }
             >
               <i className="fas fa-user-plus"></i>sign up
             </NavLink>
